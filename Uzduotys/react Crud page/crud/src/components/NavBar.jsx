8109deca7085bb69bef4b717c0b4b6ef8c27c870
InetaKr <ineta.kriunaite@outlook.com>
2023-01-18 08:42:32 +0200

    add sign in component change code accordingly

--- Uzduotys/react Crud page/crud/src/components/NavBar.jsx
+++ Uzduotys/react Crud page/crud/src/components/NavBar.jsx
@@ -1,95 +1,33 @@
 import { useState } from "react";
+import SignInForm from "./SignInForm";
 
-const NavBar = (props) => {
-  const [userLogin, setUserLogin] = useState({
-    name: '',
-    password: ''
-  });
-  const [status, setStatus] = useState('');
-  const [showForm, setShowForm] = useState(false);
+const NavBar = () => {
   const [showSignInButton, setShowSignInButton] = useState(true); //new state variable
+  const [status, setStatus] = useState('');
   
   //function to handle the sign-in button click
   const handleSignInClick = () => {
-    setShowForm(true);
     setShowSignInButton(false); //hide the sign-in button
   }
 
-  //function to handle the successful login
-  const handleSuccessfulLogin = () => {
-    setShowForm(false);
+  const handleSuccessfulLogin = (status) => {
+    setShowSignInButton(true);
+    setStatus(status);
   }
 
-  //updating name and password property
-  const handleUserInput = (e) => {
-    switch (e.target.name) {
-      case 'name':
-        setUserLogin({
-          ...userLogin,
-          name: e.target.value
-        });
-        break;
-      case 'password':
-        setUserLogin({
-          ...userLogin,
-          password: e.target.value
-        });
-        break;
-
-      default:
-        console.log('error');
-    }
-  }
-
-  //fetching from signIn.json server, comparing values of userLogin.name and userLogin.password
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const signInDuomenys = await fetch('http://localhost:5000/users')
-      .then(rez => rez.json());
-    //iterating through signIn.json
-    let isLogin = false;
-    for (let i = 0; i < signInDuomenys.length; i++) {
-      if (userLogin.name === signInDuomenys[i].name && userLogin.password === signInDuomenys[i].password) {
-        isLogin= true;
-        break;
-      }
-    }
-    if (isLogin) {
-      setStatus(`Welcome, ${userLogin.name}`);
-      handleSuccessfulLogin();
-    } else {
-      setStatus('Invalid Username or password');
-    }
-  }
   return (
     <>
       {showSignInButton && (
         <button className="sign-in" onClick={handleSignInClick}>Sign In</button>
-      )}
-      {showForm && (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            name="name"
-            value={userLogin.name}
-            onChange={handleUserInput}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={userLogin.password}
-            onChange={handleUserInput}
-          />
-          <button className="login" type="submit">Log In</button>
-        </form>
-) }
-{ status && <div>{status}</div> }
-</>
-);
-}
-export default NavBar;
+        )}
+        {!showSignInButton && (
+        <SignInForm handleSuccessfulLogin={handleSuccessfulLogin}/>
+        )}
+         { status && <div>{status}</div> }
+        </>
+        );
+        }
+export default NavBar        
 
 
 
---
+++ Uzduotys/react Crud page/crud/src/components/SignInForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const SignInForm = (props) => {
+  const [userLogin, setUserLogin] = useState({
+    name: '',
+    password: ''
+  });
+  
+  //updating name and password property
+  const handleUserInput = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setUserLogin({
+          ...userLogin,
+          name: e.target.value
+        });
+        break;
+      case 'password':
+        setUserLogin({
+          ...userLogin,
+          password: e.target.value
+        });
+        break;
+
+      default:
+        console.log('error');
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const signInDuomenys = await fetch('http://localhost:5000/users')
+      .then(rez => rez.json());
+    const user = signInDuomenys.find(user => user.name === userLogin.name && user.password === userLogin.password);
+    if (user) {
+        props.handleSuccessfulLogin("Welcome, " + userLogin.name);
+    } else {
+      props.handleSuccessfulLogin("Invalid Username or password");
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        name="name"
+        value={userLogin.name}
+        onChange={handleUserInput}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={userLogin.password}
+        onChange={handleUserInput}
+      />
+      <button className="login" type="submit">Log In</button>
+    </form>
+);
+}
+export default SignInForm
+
