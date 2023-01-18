85d114036127dbb2ed1eba1280cac3f7b419cb71
InetaKr <ineta.kriunaite@outlook.com>
2023-01-19 01:38:56 +0200

    navbar coments

--- Uzduotys/react Crud page/crud/src/components/NavBar.jsx
+++ Uzduotys/react Crud page/crud/src/components/NavBar.jsx
@@ -2,18 +2,27 @@ import { useState } from "react";
 import SignInForm from "./SignInForm";
 import companyLogo from '../images/logo.png';
 
+
+
+
 const NavBar = () => {
-  const [showSignInButton, setShowSignInButton] = useState(true); 
-  const [status, setStatus] = useState('');
+  ///komponentas turi 2 state variables kurie naudoja useState hooka
+  const [showSignInButton, setShowSignInButton] = useState(true); ///naudojamas tam kad rodyti/nerodytu Sign In buttono
+  const [status, setStatus] = useState('');//status naudojamas storinti users logino status
   
-  const handleSignInClick = () => {
+  ///komponentas turi dvi funkcijas
+ 
+
+  const handleSignInClick = () => { //iskviecima kai signIn buttonas paspaustas ir tada nustato sign in butonos stata i false, taip atidaro Sign in Forma
     setShowSignInButton(false); 
   }
 
-  const handleSuccessfulLogin = (status) => {
-    setShowSignInButton(true);
+  const handleSuccessfulLogin = (status) => { ///funkcija perduodama kaip prop Sign in formos komponentui. Ji iskvieciama kai useris sekmingai prisijungia 
+    setShowSignInButton(true); //funkcija setina showSignInButton state i true ir user state i praeita state.
     setStatus(status);
   }
+  //Vartotojo informacijos skiltyje ji pagal salyga pateikia prisijungimo mygtuką arba SignInForm komponentą pagal showSignInButton steta 
+  // taip pat pateikiamas vartotojo steitas, jei yra jo reikšmė.
 
   return (
     <>
@@ -36,6 +45,11 @@ const NavBar = () => {
 }
 
 export default NavBar        
+
+///apskritai komponentas atsakingas navigation bar aplikacija ir tvarko rpisijungimo funkcionalumą
+//rodydama sign in forma kai useris clikina sign in butona
+//ir tvarko sekminga prisijungima rodydamas welcome vartotojo vardas ir paslepdydamas sign in forma
+
 
 
 
