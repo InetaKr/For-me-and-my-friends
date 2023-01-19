f21ac48200797c4d3a477040199aa09d3b32f44c
InetaKr <ineta.kriunaite@outlook.com>
2023-01-19 02:01:12 +0200

    signinform komentinta

--- Uzduotys/react Crud page/crud/src/components/SignInForm.jsx
+++ Uzduotys/react Crud page/crud/src/components/SignInForm.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
+/// komponentas turi viena state variable-userlogin kuris yra objektas kuris laiko  user name ir password.  variable inicijuojams naudojant usestate hooka
 const SignInForm = (props) => {
   const [userLogin, setUserLogin] = useState({
     name: '',
     password: ''
   });
   
- 
+  /// komponetas turi dvi funkcijas handleUserInput ir handleSubmit. 
+  //The handleUserInput funkcija issaukiama kai useris types input fieldus ir uodatina state pagal tai.    
   const handleUserInput = (e) => {
     switch (e.target.name) {
       case 'name':
@@ -27,6 +29,10 @@ const SignInForm = (props) => {
     }
   }
 
+  ///The handleSubmit funkcija issaukiama kai useris submitina forma. tada funkcija fetchina is serverio. funkcija expects array of userio infomacijos/objektu.
+  //tada filtruoja users array ir iesko objekto su suvestais vartotojo name ir password
+  //jhei randa tinkama user tada issaukia handleSuccessfulLogin .
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const signInCredentials = await fetch('http://localhost:5000/users')
@@ -38,6 +44,7 @@ const SignInForm = (props) => {
       props.handleSuccessfulLogin("Invalid Username or password");
     }
   }
+  //returnina forma su name ir password inputais ir button login...Itaip pat prijungia formos inputus to state, ir formos submission to the handleSubmit funckcija.
   return (
     <form onSubmit={handleSubmit} className="signInForm">
       <input
@@ -60,3 +67,7 @@ const SignInForm = (props) => {
 }
 export default SignInForm
 
+///apskritai sita komponentas atsakingas uz  logino funkcioneluma
+///handlina input forma su user info... duoda requesta serveriui ir tikrina ar useris yra valid ar ne..
+///jei valid tai issaukiama  handleSuccessfulLogin funckcija is propsu kad parent elementas siu atveju NavBar zinotu kad login successful
+///jei user invalid kalina ta pacia funkcija su kitokiu argumentu kad useris zinotu kad loginas nepavyko
